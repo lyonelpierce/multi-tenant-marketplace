65d034758a66a36d63eb63d1713428171d3ed033
Lyonel Pierce <lyonel@live.com>
2024-04-29 22:01:59 -0400

    home page metadata

--- components/Landing/LandingNavbar.tsx
+++ components/Landing/LandingNavbar.tsx
@@ -4,29 +4,36 @@ import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import WidthWrapper from "@/components/WidthWrapper";
 import { HeartIcon, ShoppingBag, User } from "lucide-react";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 const LandingNavbar = () => {
   const { userId }: { userId: string | null } = auth();
 
   return (
     <div className="bg-zinc-900 h-20 flex items-center text-white">
-      <WidthWrapper className="flex justify-between">
+      <WidthWrapper className="flex justify-between items-center">
         <Link href="/">
-          <h1 className="uppercase font-light text-2xl flex ">
+          <h1 className="uppercase font-medium text-2xl flex">
             Panda
-            <span className="text-xs font-bold text-yellow-400">EC</span>
+            <span className="text-xs font-regular text-yellow-400">EC</span>
           </h1>
         </Link>
-        <div className="flex gap-10">
-          <HeartIcon />
+        <div className="flex gap-8">
+          <Button size="icon" variant="icon" className="hover:scale-110">
+            <HeartIcon />
+          </Button>
           {!userId ? (
             <SignInButton>
-              <User />
+              <Button size="icon" variant="icon" className="hover:scale-110">
+                <User />
+              </Button>
             </SignInButton>
           ) : (
             <SignOutButton />
           )}
-          <ShoppingBag />
+          <Button size="icon" variant="icon" className="hover:scale-110">
+            <ShoppingBag />
+          </Button>
         </div>
       </WidthWrapper>
     </div>
